d1bb2ba406ecffd5dce21b3df0334b531975a79c
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-08-15 21:19:43 +0300

    context settings

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Header from './components/header/Header';
+import ToDo from './components/todo/list';
+
+function App() {
+  return (
+    <div>
+      {/* <Header/> */}
+      <ToDo/>
+    </div>
+  );
+}
+
+export default App;
