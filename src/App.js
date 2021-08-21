304f0c5daefb7bf8be8205b4a0d2229c05fa200e
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-08-21 19:46:12 +0300

    auth part done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
-// import Footer from './components/Footer/Footer';
+import React, {useContext} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import ToDo from './components/ToDo/ToDo';
 import ListContext from './context/settings/context';
+import Login from './context/auth/login';
+import { If, Else, Then } from 'react-if';
+import { AuthContext } from './context/auth/context';
 
 
 function App() {
+  const authSettings = useContext(AuthContext);
+
   return (
     <>
-      <Header/>
-      <ListContext>
-        <ToDo/>
-      </ListContext>
-      {/* <Footer/> */}
+      <Router>
+        <Header/>
+        <If condition = {!authSettings.loggedIn}>
+          <Then>
+            <Login/>
+          </Then>
+          <Else>
+            <Switch>
+              <ListContext>
+                <Route exact path = '/'>
+                  <ToDo/>
+                </Route>
+              </ListContext>
+            </Switch>
+          </Else>
+        </If>
+      </Router>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Navbar, Button, Alignment} from '@blueprintjs/core';
+import { AuthContext } from '../../context/auth/context';
+import { useHistory } from 'react-router-dom';
+import { If, Else, Then } from 'react-if';
 
 function Header() {
+  const AuthSettings = useContext(AuthContext);
+  const history = useHistory();
+
+  const routeChange = () =>{
+    let path = `/`;
+    history.push(path);
+  };
+
   return (
     <div className = 'body'>
       <Navbar className = 'bp3-dark'>
         <Navbar.Group align={Alignment.LEFT}>
           <Navbar.Heading style= {{margin: '0 10rem' ,width: '45rem'}}>ToDo List Manager</Navbar.Heading>
           <Navbar.Divider />
-          <Button className="bp3-minimal" icon="home" text="Home"></Button>
-          <Button className="bp3-minimal" icon = 'person' text="Login" />
+          <If condition = {AuthSettings.loggedIn}>
+            <Then>
+              <Button onClick = {routeChange} className="bp3-minimal" icon="home" text="Home"></Button>
+              <Button className="bp3-minimal" icon = 'log-out' onClick={AuthSettings.logout}>Logout</Button>
+            </Then>
+            <Else>
+              <Button onClick = {routeChange} className="bp3-minimal" icon="home" text="Home"></Button>
+              <Button className="bp3-minimal" icon = 'log-in' text="Login" />
+            </Else>
+          </If>
         </Navbar.Group>
       </Navbar>
     </div>
